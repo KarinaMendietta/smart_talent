f29e80a82a9ddfe58a4cea12c136cdd5fca0e25f
KarinaMendietta <karinamendiettasoto@gmail.com>
2022-03-30 14:00:20 -0500

    formularios con select postulante y academico

--- src/pages/FormAcademico/index.js
+++ src/pages/FormAcademico/index.js
@@ -1,18 +1,35 @@
-import React from "react";
-import { Stack, TextField, Box, FormControl, InputLabel, Input, FormHelperText, Container } from "@mui/material";
+import { React, useState } from "react";
+import {
+  Select,
+  MenuItem,
+  Stack,
+  TextField,
+  Box,
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+  Container,
+} from "@mui/material";
+import { TextFieldsOutlined } from "@mui/icons-material";
 
-const FormAcademico = () => { 
-    return (
-    <FormControl container sx={{display: 'flex', justifyContent:"center"}}>
+const FormAcademico = () => {
+  const [academ, setAcadem] = useState("");
+
+  const handleChange = (event) => {
+    setAcadem(event.target.value);
+  };
+  return (
+    <FormControl container sx={{ display: "flex", justifyContent: "center" }}>
       <h1>Formulario academico</h1>
       <Stack
         component="form"
         sx={{
           width: "500px",
           margin: "0 auto",
-          background:'#fff',
-          padding:'2rem', 
-          borderRadius:'1rem'
+          background: "#fff",
+          padding: "2rem",
+          borderRadius: "1rem",
         }}
         spacing={2}
         noValidate
@@ -24,9 +41,117 @@ const FormAcademico = () => {
           type="text"
           variant="filled"
         />
+        <Box fullWidth>
+          <InputLabel
+            fullWidth
+            id="select-profesion-label"
+            variant="filled"
+            sx={{ color: "gray", position: "relative", top: "15px" }}
+          >
+            Area profesional
+          </InputLabel>
+          <Select
+            fullWidth
+            labelId="select-profesion-label"
+            id="select-profesion"
+            value={academ}
+            label="Profesion"
+            onChange={handleChange}
+            variant="filled"
+          >
+            <MenuItem value={"ComerVentasNeg "}>
+              Comercial, Ventas Y Negocios
+            </MenuItem>
+            <MenuItem value={"AdminContabFin "}>
+              Administracion, Contabilidad Y Finanzas
+            </MenuItem>
+            <MenuItem value={"OficiosOtros "}>Oficios Y Otros</MenuItem>
+            <MenuItem value={"TecSistTelecom"}>
+              Tecnologia, Sistemas Y Telecomunicaciones
+            </MenuItem>
+            <MenuItem value={"ProdManuf"}>
+              Produccion y Manufactura
+            </MenuItem>
+            <MenuItem
+              value={"AtencCallCentTelemark "}
+            >
+              Atencion Al Cliente, Call Center Y Telemarketing
+            </MenuItem>
+            <MenuItem value={"AbastLogist "}>
+              Abastecimiento y Logistica
+            </MenuItem>
+            <MenuItem value={"Ingenierias "}>Ingenierias</MenuItem>
+            <MenuItem value={"SaludMedicFarma"}>
+              Salud, Medicina y Farmacia
+            </MenuItem>
+            <MenuItem value={"RecHumCapacit"}>
+              Recursos Humanos y Capacitacion
+            </MenuItem>
+            <MenuItem value={"MinPetroGas "}>
+              Mineria, Petroleo Y Gas
+            </MenuItem>
+            <MenuItem value={"MarkPubli "}>
+              Marketing Y Publicidad
+            </MenuItem>
+            <MenuItem value={"IngCivilConstr"}>
+              Ingenieria Civil y Construccion
+            </MenuItem>
+            <MenuItem value={"EducDocencInvest "}>
+              Educacion, Docencia e Investigacion
+            </MenuItem>
+            <MenuItem value={"GastroTurismo "}>
+              Gastronomia Y Turismo
+            </MenuItem>
+            <MenuItem value={"Legales "}>Legales</MenuItem>
+            <MenuItem value={"DiseÑO "}>DiseÑO</MenuItem>
+            <MenuItem
+              value={"ComuniRelacionesInstPubli "}
+            >
+              Comunicacion, Relaciones Institucionales Y Publicas
+            </MenuItem>
+            <MenuItem value={"SecretariasRecepcion "}>
+              Secretarias y Recepcion
+            </MenuItem>
+            <MenuItem value={"AduanaComercioExterior "}>
+              Aduana y Comercio Exterior
+            </MenuItem>
+            <MenuItem value={"Seguros "}>Seguros</MenuItem>
+            <MenuItem value={"GerenciaDireccionGeneral "}>
+              Gerencia y Direccion General
+            </MenuItem>
+            <MenuItem value={"Enfermeria "}>Enfermeria</MenuItem>
+          </Select>
+        </Box>
+        <Box fullWidth>
+          <InputLabel
+            fullWidth
+            id="select-nivelAcademico-label"
+            variant="filled"
+            sx={{ color: "gray", position: "relative", top: "15px" }}
+          >
+            Nivel Academico
+          </InputLabel>
+          <Select
+            fullWidth
+            labelId="select-nivelAcademico-label"
+            id="select-nivelAcademico"
+            value={academ}
+            label="Nivel Academico"
+            onChange={handleChange}
+            variant="filled"
+          >
+            <MenuItem value={"Secundaria"}>Secundaria</MenuItem>
+            <MenuItem value={"Tecnico"}>Tecnico</MenuItem>
+            <MenuItem value={"Universitario"}>Universitario</MenuItem>
+            <MenuItem value={"Titulado"}>Titulado</MenuItem>
+            <MenuItem value={"Maestria"}>Maestria</MenuItem>
+            <MenuItem value={"Doctorado"}>Doctorado</MenuItem>
+
+          </Select>
+        </Box>
         <TextField
-          name="lugarEstudios"
-          label="Lugar de estudios"
+          name="centroEstudios"
+          label="Centro de estudios"
           type="text"
           variant="filled"
         />
@@ -36,10 +161,9 @@ const FormAcademico = () => {
           type="text"
           variant="filled"
         />
-        
-    </Stack>
+      </Stack>
     </FormControl>
   );
-}
-       
+};
+
 export default FormAcademico;
--- src/pages/FormPostulante/index.js
+++ src/pages/FormPostulante/index.js
@@ -1,99 +1,189 @@
-import React from "react";
-import {Select, MenuItem, Stack, TextField, Box, FormControl, InputLabel, Input, FormHelperText, Container } from "@mui/material";
+import { React, useState } from "react";
+import {  Select,
+  MenuItem,
+  Stack,
+  TextField,
+  Box,
+  FormControl,
+  InputLabel,
+  Input,
+  FormHelperText,
+  Container,
+} from "@mui/material";
 import { TextFieldsOutlined } from "@mui/icons-material";
 
-const FormPostulante = () => { 
-    return (
-      <FormControl container sx={{display: 'flex', justifyContent:"center"}}>
+const FormPostulante = () => {
+  const [dep, setDep] = useState("");
+
+  const handleChange = (event) => {
+    setDep(event.target.value);
+  };
+  return (
+    <FormControl container sx={{ display: "flex", justifyContent: "center" }}>
       <h1>Formulario de datos</h1>
       <Stack
         component="form"
         sx={{
           width: "500px",
           margin: "0 auto",
-          background:'#fff',
-          padding:'2rem', 
-          borderRadius:'1rem'
+          background: "#fff",
+          padding: "2rem",
+          borderRadius: "1rem",
         }}
         spacing={2}
         noValidate
         autoComplete="off"
       >
-        <TextField
-          name="nombre"
-          label="Nombre"
-          type="text"          
-          variant="filled"      
-        />
+        <TextField name="nombre" label="Nombre" type="text" variant="filled" />
         <TextField
           name="apellido"
           label="Apellido"
-          type="text"          
+          type="text"
           variant="filled"
         />
-        <TextField
-          name="dni"
-          label="Dni"
-          type="text"          
-          variant="filled"
-        />
-        <TextField
-          name="correo"
-          label="Correo"
-          type="mail"          
-          variant="filled"
-        />
+        <TextField name="dni" label="Dni" type="text" variant="filled" />
+        <TextField name="correo" label="Correo" type="mail" variant="filled" />
         <TextField
           name="fechaDeNacimiento"
           label="Fecha de nacimiento"
-          type="text"          
+          type="text"
           variant="filled"
         />
-        <TextField
-          name="genero"
-          label="Genero"
-          type="text"          
-          variant="filled"
-        />
+        <Box fullWidth>
+          <InputLabel
+            fullWidth
+            id="select-genero-label"
+            variant="filled"
+            sx={{ color: "gray", position: "relative", top: "15px" }}
+          >
+            Genero
+          </InputLabel>
+          <Select
+            fullWidth
+            labelId="select-genero-label"
+            id="select-genero"
+            value={dep}
+            label="Genero"
+            onChange={handleChange}
+            variant="filled"
+          >
+            <MenuItem value={"Femenino"}>Femenino</MenuItem>
+            <MenuItem value={"Masculino"}>Masculino</MenuItem>
+          </Select>
+        </Box>
         <TextField
           name="PaisDeNacimiento"
           label="Pais de nacimiento"
-          type="text"          
+          type="text"
           variant="filled"
         />
         <TextField
-           name="celular"
-           label="Celular"
-           type="text"          
-           variant="filled"
-        />
-        <TextField
-          name="departamento"
-          label="Departamento"
-          type="text"          
+          name="celular"
+          label="Celular"
+          type="text"
           variant="filled"
         />
-        <TextField
-          name="provincia"
-          label="Provincia"
-          type="text"          
-          variant="filled"
-        />
-        <TextField
-          name="distrito"
-          label="Distrito"
-          type="text"          
-          variant="filled"
-        />
+        <Box fullWidth>
+          <InputLabel
+            fullWidth
+            id="select-departamento-label"
+            variant="filled"
+            sx={{ color: "gray", position: "relative", top: "15px" }}
+          >
+            Departamento
+          </InputLabel>
+          <Select
+            fullWidth
+            labelId="select-departamento-label"
+            id="select-departamento"
+            value={dep}
+            label="Departamento"
+            onChange={handleChange}
+            variant="filled"
+          >
+            <MenuItem value={"Amazonas "}>Amazonas</MenuItem>
+            <MenuItem value={"Áncash "}>Áncash</MenuItem>
+            <MenuItem value={"Apurímac "}>Apurímac</MenuItem>
+            <MenuItem value={"Arequipa "}>Arequipa</MenuItem>
+            <MenuItem value={"Ayacucho "}>Ayacucho</MenuItem>
+            <MenuItem value={"Cajamarca "}>Cajamarca</MenuItem>
+            <MenuItem value={"Callao "}>Callao</MenuItem>
+            <MenuItem value={"Cusco "}>Cusco</MenuItem>
+            <MenuItem value={"Huancavelica "}>Huancavelica</MenuItem>
+            <MenuItem value={"Huánuco "}>Huánuco</MenuItem>
+            <MenuItem value={"Ica "}>Ica</MenuItem>
+            <MenuItem value={"Junín "}>Junín</MenuItem>
+            <MenuItem value={"La Libertad "}>La Libertad</MenuItem>
+            <MenuItem value={"Lambayeque "}>Lambayeque</MenuItem>
+            <MenuItem value={"Lima "}>Lima</MenuItem>
+            <MenuItem value={"Loreto "}>Loreto</MenuItem>
+            <MenuItem value={"Madre de Dios "}>Madre de Dios</MenuItem>
+            <MenuItem value={"Moquegua "}>Moquegua</MenuItem>
+            <MenuItem value={"Pasco "}>Pasco</MenuItem>
+            <MenuItem value={"Piura "}>Piura</MenuItem>
+            <MenuItem value={"Puno "}>Puno</MenuItem>
+            <MenuItem value={"San Martín "}>San Martín</MenuItem>
+            <MenuItem value={"Tacna "}>Tacna</MenuItem>
+            <MenuItem value={"Tumbes "}>Tumbes</MenuItem>
+            <MenuItem value={"Ucayali "}>Ucayali</MenuItem>
+          </Select>
+        </Box>
+        <Box fullWidth>
+          <InputLabel
+            fullWidth
+            id="select-provincia-label"
+            variant="filled"
+            sx={{ color: "gray", position: "relative", top: "15px" }}
+          >
+            Provincia
+          </InputLabel>
+          <Select
+            fullWidth
+            labelId="select-provincia-label"
+            id="select-provincia"
+            value={dep}
+            label="Provincia"
+            onChange={handleChange}
+            variant="filled"
+          >            
+            <MenuItem value={"Abancay "}>Abancay</MenuItem>
+            <MenuItem value={"Arequipa "}>Arequipa</MenuItem>
+            <MenuItem value={"Ayacucho "}>Ayacucho</MenuItem>
+            <MenuItem value={"Cajamarca "}>Cajamarca</MenuItem>
+            <MenuItem value={"Callao "}>Callao</MenuItem>
+            <MenuItem value={"Cerro de Pasco "}>Cerro de Pasco</MenuItem>
+            <MenuItem value={"Chachapoyas "}>Chachapoyas</MenuItem>
+            <MenuItem value={"Chiclayo "}>Chiclayo</MenuItem>
+            <MenuItem value={"Cuzco "}>Cuzco</MenuItem>
+            <MenuItem value={"Huancavelica "}>Huancavelica</MenuItem>
+            <MenuItem value={"Huancayo "}>Huancayo</MenuItem>
+            <MenuItem value={"Huánuco "}>Huánuco</MenuItem>
+            <MenuItem value={"Huaraz "}>Huaraz</MenuItem>
+            <MenuItem value={"Ica "}>Ica</MenuItem>
+            <MenuItem value={"Iquitos "}>Iquitos</MenuItem>
+            <MenuItem value={"Lima "}>Lima</MenuItem>
+            <MenuItem value={"Moquegua "}>Moquegua</MenuItem>
+            <MenuItem value={"Moyobamba "}>Moyobamba</MenuItem>
+            <MenuItem value={"Piura "}>Piura</MenuItem>
+            <MenuItem value={"Pucallpa "}>Pucallpa</MenuItem>
+            <MenuItem value={"Puerto Maldonado "}>Puerto Maldonado</MenuItem>
+            <MenuItem value={"Puno "}>Puno</MenuItem>
+            <MenuItem value={"Tacna "}>Tacna</MenuItem>
+            <MenuItem value={"Trujillo "}>Trujillo</MenuItem>
+            <MenuItem value={"Tumbes "}>Tumbes</MenuItem>
+            <MenuItem value={"Otro "}>Otro</MenuItem>
+          </Select>
+        </Box>       
+
         <TextField
           name="direccion"
           label="Direccion"
-          type="text"          
+          type="text"
           variant="filled"
         />
       </Stack>
     </FormControl>
   );
-}
+};
 
 export default FormPostulante;
