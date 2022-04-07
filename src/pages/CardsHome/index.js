747e6d89071a840dcdb7346636e6400178d6230e
willyPatino <wbps85@gmail.com>
2022-04-06 22:46:35 -0500

    enviando ID para el localstorage

--- src/pages/CardsHome/index.js
+++ src/pages/CardsHome/index.js
@@ -1,9 +1,9 @@
 //Importando Hooks
-import { useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 //Importando de firestore
-import { getAnnouncements } from "../../service/firestore";
+import {getAnnouncements} from "../../service/firestore";
 // Importando Font Awesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
   faLocationDot,
   faUser,
@@ -30,7 +30,13 @@ const CardsHome = () => {
   const fetchAnnouncements = async () => {
     const data = await getAnnouncements();
     setAnnouncements(data);
-    console.log(data)
+    console.log(data);
+  };
+
+  // Para enviar el ID al localstorage
+  const handleClickPostulate = (id) => {
+    console.log(id);
+    localStorage.setItem("id_convocatoria", id);
   };
 
   //Inicializando los fetch
@@ -38,25 +44,44 @@ const CardsHome = () => {
     fetchAnnouncements();
   }, []);
   return (
-    <Container >
+    <Container>
       <h2 className="cards-title">Ofertas Laborales</h2>
       <Grid container spacing={3}>
         {announcements.length > 0 &&
           announcements.map((announcement, index) => (
-            <Grid key={index} item md={6} lg={6} sm={12} xs={12} style={{minHeight:'730px'}}>
-              <Card className="card" sx={{ borderRadius: "2rem" }} >
+            <Grid
+              key={index}
+              item
+              md={6}
+              lg={6}
+              sm={12}
+              xs={12}
+              style={{minHeight: "730px"}}
+            >
+              <Card className="card" sx={{borderRadius: "2rem"}}>
                 <CardMedia
                   component="img"
                   className="img-pokemon"
                   image={announcement.photo_convocatoria}
-                  sx={{height:'25rem'}}
+                  sx={{height: "25rem"}}
                 />
-                <CardContent className="card__content" sx={{display:'flex', flexDirection:'column', justifyContent:'space-between'}}>
-                  <h3 className="card__name">{announcement.nombre_convocatoria}</h3>
+                <CardContent
+                  className="card__content"
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <h3 className="card__name">
+                    {announcement.nombre_convocatoria}
+                  </h3>
 
                   <Grid container spacing={3} mb={2}>
                     <Grid item md={12} lg={12} sm={12} xs={12} mb={2}>
-                        <p className="card__description">{announcement.descripcion}</p>
+                      <p className="card__description">
+                        {announcement.descripcion}
+                      </p>
                     </Grid>
 
                     <Grid item md={6} lg={6} sm={4} xs={6} mb={2}>
@@ -68,7 +93,10 @@ const CardsHome = () => {
                     </Grid>
 
                     <Grid item md={6} lg={6} sm={4} xs={6} mb={2}>
-                      <FontAwesomeIcon icon={faUser} className="icon--card" />
+                      <FontAwesomeIcon
+                        icon={faUser}
+                        className="icon--card"
+                      />
                       <span className="card__span">12 Vacantes</span>
                     </Grid>
 
@@ -78,7 +106,7 @@ const CardsHome = () => {
                         className="icon--card"
                       />
                       <span className="card__span">
-                        01/01/2022 - 20/01/2022
+                        01/01/2022 al 20/01/2022
                       </span>
                     </Grid>
 
@@ -91,11 +119,17 @@ const CardsHome = () => {
                     </Grid>
                   </Grid>
 
+                  {/* Para enviar el ID al localstorage */}
                   <Button
                     fullWidth
                     variant="contained"
                     color="primary"
-                    sx={{ fontSize: "1.6rem", borderRadius: "1rem" }}
+                    onClick={() =>
+                      handleClickPostulate(
+                        announcement.id_convocatoria
+                      )
+                    }
+                    sx={{fontSize: "1.6rem", borderRadius: "1rem"}}
                   >
                     Postular
                   </Button>
