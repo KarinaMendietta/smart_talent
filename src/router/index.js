98be5b39df070012b096fcbccf8543f470c762b6
sskaren1 <karen.93st@gmail.com>
2022-04-07 21:20:25 -0500

    router, firestore

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,11 @@ import Home from "./../pages/Home";
 import SignIn from "./../pages/SignIn";
 import CardsHome from "./../pages/CardsHome";
 import Dashboard from "./../pages/Dashboard";
-import Tables from "./../pages/Tables";
-
-
-// import FormPostulante from "../pages/FormPostulante";
-// import FormAcademico from "../pages/FormAcademico";
-// import FormLaboral from "../pages/FormLaboral";
-// import FormPsicologico from "../pages/FormPsicologico";
-
+import Tables from "./../pages/Tables"
+import FormPostulante from "../pages/FormPostulante";
+import FormAcademico from "../pages/FormAcademico";
+import FormLaboral from "../pages/FormLaboral";
+import FormPsicologico from "../pages/FormPsicologico";
 // Importando Layouts
 import Main from "./../layouts/Main"
 import Private from "./../layouts/Private";
@@ -25,14 +22,14 @@ const Router = () => {
         {/* Route para el usuario  */}
         <Route path="/" element={<Home />} />
         <Route path="/cards-home" element={<CardsHome />} />
-
-        {/* <Route path="/form-postulante" element={<FormPostulante />} />
+        <Route path="/form-postulante" element={<FormPostulante />} />
         <Route path="/form-academico" element={<FormAcademico />} />
         <Route path="/form-laboral" element={<FormLaboral />} />
-        <Route path="/form-psicologico" element={<FormPsicologico />} /> */}
+        <Route path="/form-psicologico" element={<FormPsicologico />} />
 
         {/* Route para la empresa  */}
         <Route path="/sign-in" element={<SignIn />} />
+
 
         {/* Route (Privadas) */}
         {/* Route del usuario RRHH logeado*/}
--- src/service/firestore.js
+++ src/service/firestore.js
@@ -9,13 +9,9 @@ import {
   updateDoc,
   deleteDoc,
 } from "firebase/firestore/lite";
-
 import {
   getAuth,
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
-  updateProfile,
-  sendEmailVerification,
 } from "firebase/auth";
 import { v4 as uuidv4 } from "uuid";
 
@@ -28,36 +24,39 @@ const firebaseConfig = {
   appId: "1:1036928853189:web:3e64b1b0dbc0dc465f3cac",
   measurementId: "G-06B162BMY2"
 };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  //const analytics = getAnalytics(app);
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
 
-// database : base de datos
-const db = getFirestore(app);
 
-// Hacer la petición para poder traer datos de tblEmpresa
-export const getUsersAdmin = async () => {
-  // paso 1: Traer la coleccion de datos
-  const collectionEmpresa = collection(db, "empresa");
-  // paso 2: Traer los documentos
-  const documentUsers = await getDocs(collectionEmpresa);
-  // paso 3: Crear un arreglo que guarde los documentos que estamos obteniendo
-  const usersAdmin = documentUsers.docs.map((doc) => doc.data());
-  return usersAdmin;
-};
+  // Hacer la peticion para poder traer las preguntas
+  export const getTests= async () => {
+    // paso 1: Traer la coleccion de datos
+    const collectionTests = collection(db, "test");
+    // paso 2: Traer los documentos
+    const documentTests = await getDocs(collectionTests);
+    // paso 3: Crear un arreglo que guarde los documentos que estamos obteniendo
+    const tests = documentTests.docs.map((doc) => doc.data());
+    //console.log("mira",tests)
+    return tests;
+  };
 
-// Hacer la petición para poder traer datos de tblPostulantes
-export const getApplicants = async () => {
-  // paso 1: Traer la coleccion de datos
-  const collectionApplicants = collection(db, "postulante");
-  // paso 2: Traer los documentos
-  const documentApplicants = await getDocs(collectionApplicants);
-  // paso 3: Crear un arreglo que guarde los documentos que estamos obteniendo
-  const usersApplicants = documentApplicants.docs.map((doc) => doc.data());
-  return usersApplicants;
-};
+  export const getPostulantes= async () => {
+    // paso 1: Traer la coleccion de datos
+    const collectionPostulantes = collection(db, "postulante");
+    // paso 2: Traer los documentos
+    const documentPostulantes = await getDocs(collectionPostulantes);
+    // paso 3: Crear un arreglo que guarde los documentos que estamos obteniendo
+    const postulantes = documentPostulantes.docs.map((doc) => doc.data());
+    //console.log("postulantes",postulantes)
+    //solo enviamos el numero de postulantes
+    return postulantes.length;
+  }
 
-// Hacer la petición para poder traer datos de tblConvocatoria
+  // Hacer la petición para poder traer datos de tblConvocatoria
 export const getAnnouncements = async () => {
   // paso 1: Traer la coleccion de datos
   const collectionAnnouncement = collection(db, "convocatoria");
@@ -122,3 +121,46 @@ export const loginUser = async (email, password) => {
     };
   }
 };
+
+  export const registerPostulante = async (idPostulante,postulante) => {
+    const id = uuidv4().replaceAll("-", "");
+    postulante.id = id;
+    postulante.id_Postulante = idPostulante;
+    await setDoc(doc(db, "postulante", id), postulante);
+  };
+
+  export const registerAcademico = async (idPostulante,academico) => {
+    const id = uuidv4().replaceAll("-", "");
+    academico.id_academico = id;
+    academico.id_postulante = idPostulante;
+    await setDoc(doc(db, "academico", id), academico);
+  };
+
+  export const registerLaboral = async (idPostulante,laboral) => {
+    const id = uuidv4().replaceAll("-", "");
+    laboral.id_laboral = id;
+    laboral.id_postulante = idPostulante;
+    await setDoc(doc(db, "laboral", id), laboral);
+  };
+
+  export const registerPsicologico = async (idPostulante,psicologico) => {
+    const id = uuidv4().replaceAll("-", "");
+    psicologico.id_psicologico = id;
+    psicologico.id_postulante = idPostulante;
+    await setDoc(doc(db, "psicologico", id), psicologico);
+  };
+
+  export const registerCalificacion = async (idPostulante,idConvocatoria,calificacion) => {
+    const id = uuidv4().replaceAll("-", "");
+    calificacion.id_calificacion = id;
+    calificacion.id_postulante = idPostulante;
+    calificacion.id_convocatoria = idConvocatoria
+    await setDoc(doc(db, "calificacion", id), calificacion);
+  };
+
+ // actualizar un datos en firebase
+ export const updateCalificacion = async (calificacion) => {
+  const calificacionRef = doc(db, "calificacion", calificacion.id);
+  await updateDoc(calificacionRef, calificacion);
+};
+
